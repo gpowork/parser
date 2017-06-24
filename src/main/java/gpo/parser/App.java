750abfefe85ecd63bd376e4fd5a5770eb697f9cb
gpowork <gpowork@gmail.com>
2017-06-24 08:09:07 +0300

    Initial commit.

---
+++ src/main/java/gpo/parser/App.java
@@ -0,0 +1,13 @@
+package gpo.parser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
